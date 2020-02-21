4ee7bc9c25953f2f2dc8fa70a8d20109e6c71f36
bsaisudh <bsaisudh@terpmail.umd.edu>
2020-02-21 11:20:41 -0500

    [WIP] HW3 - just started

--- Assignments/HW3/HW3.py
+++ Assignments/HW3/HW3.py
@@ -8,35 +8,40 @@ import cv2
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 25
-rawCapture = PiRGBArray(camera, size=(640,480))
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
 # allow the camera to warmup
 time.sleep(0.1)
 
 # define the codec and create VideoWriter object
 # fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-# out = cv2.VideoWriter(‘videoname.avi’, fourcc, 10, (640, 480))
-out = cv2.VideoWriter('video.avi',cv2.VideoWriter_fourcc(*"XVID"), 2, (1280,720))
-# keep looping
+# out = cv2.VideoWriter(‘videoname.avi’, fourcc, 10, (1280, 720))
+out = cv2.VideoWriter('video.avi',cv2.VideoWriter_fourcc(*"XVID"), 2, (640, 480))
+
+# initialize  timing metrics variables
 start = time.time()
 video_start_time = time.time()
+
+# keep looping
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=False):
 	# grab the current frame
 	image = frame.array
-	# show the frame to our screen
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
+	# show the frame to our screen
+	image = cv2.flip(image, -1)
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
 	# write frame to video file
-	out.writimage)
+	out.write(image)
 	# press the 'q' key to stop the video stream
 	if key == ord("q"):
 		break
 	print(f'--> {time.time()-start} ; {time.time()-video_start_time} ')
-	if time.time() - video_start_time > 30:
+	if time.time() - video_start_time > 40:
 		break
 	start = time.time()
 
 out.release()
-camera.close()
-cv2.
+camera.close().
+

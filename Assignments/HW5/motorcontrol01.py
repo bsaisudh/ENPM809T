1703c2ff5d5fb1b0b3f2a919649fb55b60174281
Rama Prashanth <rv@ramaprashanth.com>
2020-03-05 18:15:33 -0500

    motorcontrol hw5

--- Assignments/HW5/motorcontrol01.py
+++ Assignments/HW5/motorcontrol01.py
@@ -1,2 +1,37 @@
 import RPi.GPIO as gpio
 import time
+
+
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(31, gpio.OUT) #IN1
+    gpio.setup(33, gpio.OUT) #IN1
+    gpio.setup(35, gpio.OUT) #IN1
+    gpio.setup(37, gpio.OUT) #IN1
+
+
+def gameover():
+    # Set all pins LOW
+    gpio.output(31, False)
+    gpio.output(33, False)
+    gpio.output(35, False)
+    gpio.output(37, False)
+
+
+def forward(tf):
+    init()
+    # Left wheels
+    gpio.output(31, True)
+    gpio.output(33, True)
+    # Right wheels
+    gpio.output(35, True)
+    gpio.output(37, True)
+    # Wait
+    time.sleep(tf)
+    # Set all pins low and cleanup
+    gameover()
+    gpio.cleanup()
+    
+
+if __name__ == '__main__':
+    forward(2)
---
+++ Assignments/HW5/motorcontrol02.py
@@ -0,0 +1,104 @@
+import RPi.GPIO as gpio
+import time
+
+
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(31, gpio.OUT) #IN1
+    gpio.setup(33, gpio.OUT) #IN1
+    gpio.setup(35, gpio.OUT) #IN1
+    gpio.setup(37, gpio.OUT) #IN1
+
+
+def gameover():
+    # Set all pins LOW
+    gpio.output(31, False)
+    gpio.output(33, False)
+    gpio.output(35, False)
+    gpio.output(37, False)
+
+
+def forward(tf):
+    init()
+    # Left wheels
+    gpio.output(31, True)
+    gpio.output(33, True)
+    # Right wheels
+    gpio.output(35, True)
+    gpio.output(37, True)
+    # Wait
+    time.sleep(tf)
+    # Set all pins low and cleanup
+    gameover()
+    gpio.cleanup()
+
+
+def reverse(tf):
+    init()
+    # Left wheels
+    gpio.output(31, True)
+    gpio.output(33, True)
+    # Right wheels
+    gpio.output(35, True)
+    gpio.output(37, True)
+    # Wait
+    time.sleep(tf)
+    # Set all pins low and cleanup
+    gameover()
+    gpio.cleanup()
+
+
+def pivotleft(tf):
+    init()
+    # Left wheels
+    gpio.output(31, True)
+    gpio.output(33, True)
+    # Right wheels
+    gpio.output(35, True)
+    gpio.output(37, True)
+    # Wait
+    time.sleep(tf)
+    # Set all pins low and cleanup
+    gameover()
+    gpio.cleanup()
+
+
+def pivotright(tf):
+    init()
+    # Left wheels
+    gpio.output(31, True)
+    gpio.output(33, True)
+    # Right wheels
+    gpio.output(35, True)
+    gpio.output(37, True)
+    # Wait
+    time.sleep(tf)
+    # Set all pins low and cleanup
+    gameover()
+    gpio.cleanup()
+
+
+def key_input(event):
+    init()
+    print("Key: ", event)
+    key_press = event
+    tf = 1
+    
+    if key_press.lower() == 'w':
+        forward(tf)
+    elif key_press.lower() == 's':
+        reverse(tf)
+    elif key_press.lower() == 'a':
+        pivotleft(tf)
+    elif key_press.lower() == 'd':
+        pivotright(tf)
+    else:
+        print("Invalid key pressed!")
+
+if __name__ == '__main__':
+
+    while True:
+        key_press = input("Select driving mode: ")
+        if key_press == 'q':
+            break
+        key_input(key_press)

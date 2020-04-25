6d83f2e643fc40eb3154d0c65417f797f45063c9
bsaisudh <bsaisudh@terpmail.umd.edu>
2020-04-24 22:04:49 -0400

    HW7 plots

--- Assignments/HW7/encodercontrol05.py
+++ Assignments/HW7/encodercontrol05.py
@@ -15,19 +15,84 @@ motor.init_pwm_mode()
 print("motors set to pwm opereation")
 
 encoder = encoder_class()
+print("encoder inititalized")
+
 encoder.init_callback(motor.set_dutycycle,
                       [[None, None, 0, 0]],
                       motor.set_dutycycle,
                       [[0, 0, None, None]])
-print("encoder inititalized")
 
-motor.pwm_drive("forward", 14)
-dataR = None
-dataL = None
-encoder.countRL_till(90, 90, dataR, dataL)
+dataR = []
+dataL = []
+
+motor.pwm_drive("forward", 50)
+encoder.countRL_till(97, 97, dataR, dataL)
 motor.pwm_gameover()
 
-plt.figure()
+fig = plt.figure()
+fig.suptitle("Forward")
+plt.subplot(211)
+plt.plot(dataR)
+plt.ylabel("Right Encoder")
+
+plt.subplot(212)
+plt.plot(dataL)
+plt.ylabel("Left Encoder")
+
+time.sleep(1)
+
+dataR = []
+dataL = []
+
+motor.pwm_drive("reverse", 50)
+encoder.countRL_till(97, 97, dataR, dataL)
+motor.pwm_gameover()
+
+fig = plt.figure()
+fig.suptitle("Reverse")
+plt.subplot(211)
+plt.plot(dataR)
+plt.ylabel("Right Encoder")
+
+plt.subplot(212)
+plt.plot(dataL)
+plt.ylabel("Left Encoder")
+
+time.sleep(1)
+
+encoder.init_callback(motor.set_dutycycle,
+                      [[0, 0, 0, 0]],
+                      motor.set_dutycycle,
+                      [[None, None, None, None]])
+
+dataR = []
+dataL = []
+
+motor.pwm_drive("pivotright", 75)
+encoder.countRL_till(30, 0, dataR, dataL)
+motor.pwm_gameover()
+
+fig = plt.figure()
+fig.suptitle("Pivot Right")
+plt.subplot(211)
+plt.plot(dataR)
+plt.ylabel("Right Encoder")
+
+plt.subplot(212)
+plt.plot(dataL)
+plt.ylabel("Left Encoder")
+
+time.sleep(1)
+
+dataR = []
+dataL = []
+
+motor.pwm_drive("pivotleft", 75)
+encoder.countRL_till(30, 0, dataR, dataL)
+motor.pwm_gameover()
+
+fig = plt.figure()
+fig.suptitle("Pivot Left")
 plt.subplot(211)
 plt.plot(dataR)
 plt.ylabel("Right Encoder")
